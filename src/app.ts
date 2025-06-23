7b76c757c986eb630627be92a3ee7f9bd9b55237
saidulislam28 <saidulislam79028@gmail.com>
2025-06-23 22:35:00 +0600

    update

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,17 @@
 import express, { Application } from "express";
+import cors from "cors";
 import { booksRoutes } from "./app/controller/books.controller";
 import { borrowRoutes } from "./app/controller/borrow.controller";
 
 const app: Application = express();
-
+app.use(cors());
 app.use(express.json());
 
 app.use("/books", booksRoutes);
-app.use('/borrow', borrowRoutes)
+app.use("/borrow", borrowRoutes);
 
 app.get("/", (req, res) => {
-  res.send({appRunning: true, message: "App run successfully!!!!!"});
+  res.send({ appRunning: true, message: "App run successfully!!!!!" });
 });
 
 export default app;
